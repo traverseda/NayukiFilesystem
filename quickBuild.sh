b4d4a766c1cd88eb022a03fb8e6e8314788a2d7d
Alex Davies <traverse.da@gmail.com>
2018-06-28 13:57:09 -0300

    Bumped quickbuild script

--- quickBuild.sh
+++ quickBuild.sh
@@ -1,4 +1,5 @@
 #! /bin/sh -e
-capnp compile -onim capnpSchema/files.capnp > capnpSchema/files.nim
-capnp compile -onim capnpSchema/tags.capnp > capnpSchema/tags.nim
-nim c --nimcache:".nimcache" --threads:on -o:nayukiFsService nayukiFsService.nim && rm -rf .nimcache
+capnp compile -onim capnpSchema/NayukiFilesystem.capnp > capnpSchema/NayukiFilesystem_schema.nim
+capnp compile -onim capnpSchema/files.capnp > capnpSchema/files_schema.nim
+capnp compile -onim capnpSchema/tags.capnp > capnpSchema/tags_schema.nim
+nim c --nimcache:".nimcache" --threads:on -p:./ -o:nayukiFsService nayukiFsService.nim && rm -rf .nimcache
